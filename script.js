9d3d7522ef7eebacdbf71bd86e4b42458c1d130e
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-23 11:45:39 +0530

    Injecting List values

---
+++ script.js
@@ -0,0 +1,46 @@
+// To do list 
+
+let input = document.querySelector('#input');
+let add = document.querySelector('#add');
+let addnew1 = document.querySelector('.edit');
+
+add.addEventListener('click', (event) => {
+    event.preventDefault();
+    addlist();
+});
+
+function addlist(){
+    let text = input.value.trim();
+    if (text !== '') {
+        let addnew = document.createElement('div');
+        addnew.className = 'inside';
+
+        let ol = document.createElement('ol');
+        ol.textContent = text;
+
+        let del = document.createElement('button');
+        del.className = 'btn hover';
+        del.innerHTML = '<i class="fa-solid fa-trash" id="Delete"></i></i>';
+        del.addEventListener('click', () => { 
+            
+        });
+
+        let edit = document.createElement('button');
+        del.className = ('btn-hover');
+        edit.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>'
+        edit.style.borderTopRightRadius = '1rem';
+        edit.style.borderBottomRightRadius = '1rem';
+        edit.addEventListener('click', () => { });
+
+        addnew.appendChild(ol);
+        addnew.appendChild(del);
+        addnew.appendChild(edit);
+
+        addnew1.append(addnew);
+        console.log(addnew);
+        input.value = '';
+    }
+    else{
+        alert('Please enter a task');
+    }
+}
